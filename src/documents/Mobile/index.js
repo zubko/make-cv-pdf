73e828716ec165bc866c2984fc06bff47a4976cc
Alexander Zubko <alexander.zubko@gmail.com>
2021-02-23 20:17:25 +0200

    Change output path

--- src/documents/Mobile/index.js
+++ src/documents/Mobile/index.js
@@ -2,5 +2,4 @@
 
 export { default } from "./CV";
 
-export const buildPath =
-  "../zubko.io/static/CV-Alexander-Zubko-iOS-Android-ReactNative.pdf";
+export const buildPath = "../zubko.io/static/CV-Alexander-Zubko-Mobile-Web.pdf";
